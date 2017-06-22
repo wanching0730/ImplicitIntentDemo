947fc5b31dee36f4e71cd753ffeabb4bf047cd27
Wan Ching <wanching_97@hotmail.com>
2017-06-22 14:40:29 +0800

    git add

---
+++ app/src/main/java/com/example/android/implicitintentdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.implicitintentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+
+        EditText etMessage = (EditText) findViewById(R.id.message);
+        String message = etMessage.getText().toString();
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("text/plain");
+        startActivity(intent);
+
+    }
+}
